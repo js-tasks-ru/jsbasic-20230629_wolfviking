af5dbe0932ed522fab10de673f0bdfad07424852
Yuriy Perezhogin <wolfviking@yandex.ru>
2023-07-05 21:07:47 +0500

    update Task4 mod1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,15 +1,10 @@
-let str = 'Быть или не  быть вот в xxxxxx  чём вопрос.';
-
 
 function checkSpam(str) {
-  if (str.includes('1xBet') || str.includes('xxx') || str.includes('XXX')) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
-
+  let low = str.toLowerCase();
+  let res = low.includes("xxx") || low.includes("xbet");
+  return res;
 
 }
 
-checkSpam(str);
+
 
