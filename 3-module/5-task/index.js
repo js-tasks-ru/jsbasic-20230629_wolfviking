d939d881c0f29e1c656f7bb2f085b523c2ddae78
Yuriy Perezhogin <wolfviking@yandex.ru>
2023-07-12 16:56:40 +0500

    add module3 task5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,27 @@
 function getMinMax(str) {
-  // ваш код...
+  const toArr = inputData.split(" ");
+  const res = toArr
+    .filter((num) => {
+      return Number(num);
+    })
+    .map((el) => {
+      return Number(el);
+    });
+
+  let min = res[1];
+  let max = 0;
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] > max) {
+      max = res[i];
+    }
+    if (res[i] < min) {
+      min = res[i];
+    }
+  }
+  const result = {
+    min: min,
+    max: max,
+  };
+
+  return result;
 }
