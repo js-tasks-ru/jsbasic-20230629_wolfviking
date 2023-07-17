52c6b95d0eb9f514910e6d74185009397bc02d44
Yuriy Perezhogin <wolfviking@yandex.ru>
2023-07-17 12:17:16 +0500

    add module4 task3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  let tbody = table.getElementsByTagName("tbody")[0];
+  let trs = tbody.getElementsByTagName("tr");
+
+  for (let i = 0; i < trs.length; i++) {
+    let statusCell = trs[i].cells[3];
+    if (!statusCell.hasAttribute("data-available")) {
+      trs[i].setAttribute("properties", "hidden");
+    } else if (statusCell.dataset.available === "true") {
+      trs[i].classList.add("available");
+    } else {
+      trs[i].classList.add("unavailable");
+    }
+  }
+
+  for (let i = 0; i < trs.length; i++) {
+    let genderCell = trs[i].cells[2];
+    if (genderCell.textContent === "m") {
+      trs[i].classList.add("male");
+    } else if (genderCell.textContent === "f") {
+      trs[i].classList.add("female");
+    }
+  }
+
+  for (let i = 0; i < trs.length; i++) {
+    let ageCell = trs[i].cells[1];
+    if (+ageCell.textContent < 18) {
+      ageCell.parentElement.style.textDecoration = "line-through";
+    }
+  }
+
+  console.log(trs);
 }
