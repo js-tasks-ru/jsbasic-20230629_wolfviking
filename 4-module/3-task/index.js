b833ffbef27cb48c4f9d545f8646b5172b9e105b
Yuriy Perezhogin <wolfviking@yandex.ru>
2023-07-17 18:39:22 +0500

    update module4 task3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,39 +1,37 @@
 function highlight(table) {
-  let tbody = table.getElementsByTagName("tbody")[0];
-  let trs = tbody.getElementsByTagName("tr");
+  const actions = {
+    [THIRD_COLUMN]: (root, td) => {
+      if (td.dataset.available === "true") {
+        root.classList.toggle("available", true);
+      } else if (td.dataset.available === "false") {
+        root.classList.toggle("unavailable", true);
+      } else if (!td.hasAttribute("data-available")) {
+        root.hidden = true;
+      }
+    },
+    [SECOND_COLUMN]: (root, td) => {
+      if (td.textContent === "m") {
+        root.classList.toggle("male", true);
+      } else if (td.textContent === "f") {
+        root.classList.toggle("female", true);
+      }
+    },
+    [FIRST_COLUMN]: (root, td) => {
+      const age = parseInt(td.textContent, 10);
 
-  for (let i = 0; i < trs.length; i++) {
-    //1
-    let statusCell = trs[i].cells[3];
-    if (statusCell.dataset.available === "true") {
-      statusCell.classList.add("available");
-    }
+      if (age < 18) {
+        root.style.textDecoration = "line-through";
+      }
+    },
+  };
 
-    if (statusCell.dataset.available === "false") {
-      statusCell.classList.add("unavailable");
-    }
+  for (const tr of table.rows) {
+    Array.from(tr.cells).forEach((td, index) => {
+      const fn = actions[index];
 
-    //2
-    if (!statusCell.dataset.available) {
-      trs[i].setAttribute("hidden", "");
-    }
-
-    //3
-    let genderCell = trs[i].cells[2];
-    if (genderCell.innerHTML === "m") {
-      trs[i].classList.add("male");
-    }
-
-    if (genderCell.innerHTML === "f") {
-      trs[i].classList.add("female");
-    }
-
-    //4
-    let ageCell = trs[i].cells[1];
-    if (+ageCell.innerHTML < 18) {
-      trs[i].style.textDecoration = "line-through";
-    }
-
-    console.log(trs);
+      if (typeof fn === "function") {
+        fn(tr, td);
+      }
+    });
   }
 }
